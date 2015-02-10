ac82c20bded2782cb68ad750189c25fc82d0e159
Yashraj R. Sontakke <sontakkeyashraj@gmail.com>
2015-02-09 20:13:52 -0600

    Create employee database from string input

---
+++ src/main/java/practice/problems/Employee.java
@@ -0,0 +1,52 @@
+package practice.problems;
+
+/**
+ * Employee object to hold employee information.
+ *
+ * @author Yashraj R. Sontakke
+ */
+public class Employee {
+    private final Integer id;
+    private final String name;
+    private final String city;
+
+    /**
+     * The constructor for {@link Employee}
+     *
+     * @param id   the employee id
+     * @param name the employee name
+     * @param city the employee city
+     */
+    public Employee(final Integer id, final String name, final String city) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+    }
+
+    /**
+     * Retrieves the employee id.
+     *
+     * @return the employee id.
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * Retrieves the employee name.
+     *
+     * @return the employee name.
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * Retrieves the employee city.
+     *
+     * @return the employee city.
+     */
+    public String getCity() {
+        return city;
+    }
+}
---
+++ src/main/java/practice/problems/EmployeeDb.java
@@ -0,0 +1,46 @@
+package practice.problems;
+
+import java.util.ArrayList;
+
+/**
+ * Store string input of employee details into employee objects.
+ *
+ * @author Yashraj R. Sontakke
+ */
+public class EmployeeDb {
+
+    final ArrayList<Employee> employees = new ArrayList<Employee>();
+
+    /**
+     * Adds employees to the employee list.
+     *
+     * @param input list of employees to be added.
+     */
+    public void addEmployees(final String input) {
+        final String[] employeeArray = input.split(";");
+
+        for (final String singleEmployee : employeeArray) {
+            final String[] employeeDetails = singleEmployee.split(",");
+            final Employee employee = new Employee(Integer.parseInt(employeeDetails[0]), employeeDetails[1], employeeDetails[2]);
+            employees.add(employee);
+        }
+
+    }
+
+    /**
+     * Prints the employee list.
+     */
+    public void printEmployees() {
+        for (final Employee employee : employees) {
+            System.out.println(String.format("%d-%s-%s", employee.getId(), employee.getName(), employee.getCity()));
+        }
+    }
+
+    public static void main(String[] args) {
+        EmployeeDb employeeDb = new EmployeeDb();
+        String input = "1,emp1,city;2,emp2,city2;3,emp3,city3;4,emp4,city4";
+        employeeDb.addEmployees(input);
+        employeeDb.printEmployees();
+    }
+
+}
