c98c53e7ac6a11c7be6854e1d21b1dfffffecc82
Yashraj R. Sontakke <sontakkeyashraj@gmail.com>
2015-02-15 21:20:06 -0600

    Implementation of bubble sort

--- src/main/java/practice/algorithms/BubbleSort.java
+++ src/main/java/practice/algorithms/BubbleSort.java
@@ -1,11 +1,36 @@
 package practice.algorithms;
 
+import java.util.Arrays;
+
 /**
- * Created with IntelliJ IDEA.
- * User: yashrajsontakke
- * Date: 2/15/15
- * Time: 9:02 PM
- * To change this template use File | Settings | File Templates.
+ * Implements bubble sort.
+ * Time complexity O(n2) and space complexity O(1).
+ *
+ * @author Yashraj R. Sontakke
  */
 public class BubbleSort {
+
+    public Integer[] sort(Integer[] input) {
+        boolean swapped = true;
+
+        while (swapped) {
+            swapped = false;
+            for (int i = 1; i < input.length; i++) {
+                if (input[i - 1] > input[i]) {
+                    int temp = input[i];
+                    input[i] = input[i - 1];
+                    input[i - 1] = temp;
+                    swapped = true;
+                }
+            }
+        }
+        return input;
+    }
+
+    public static void main(String[] args) {
+        BubbleSort m = new BubbleSort();
+        Integer[] input = {4, 2, 10, 3, 5, 1, 9, 7};
+        Integer[] output = m.sort(input);
+        System.out.println("Sorted = " + Arrays.asList(output).toString());
+    }
 }
