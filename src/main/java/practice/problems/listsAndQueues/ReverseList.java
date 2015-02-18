597578954bf1023997d45ee8c7d2b1ad6544ffb0
Yashraj R. Sontakke <sontakkeyashraj@gmail.com>
2015-02-17 23:20:41 -0600

    Reverse a list

---
+++ src/main/java/practice/problems/listsAndQueues/ReverseList.java
@@ -0,0 +1,43 @@
+package practice.problems.listsAndQueues;
+
+import practice.dataStructures.queue.Node;
+import practice.dataStructures.queue.Queue;
+
+/**
+ * Returns the reverse list.
+ *
+ * @author Yashraj R. Sontakke
+ */
+public class ReverseList {
+
+    public Queue reverse(Queue input) {
+        Queue output = new Queue();
+        Node n = input.first;
+        while (n != null) {
+            if (output.first == null) {
+                output.first = new Node(n.getData());
+                output.last = output.first;
+            } else {
+                Node newNode = new Node(n.getData());
+                newNode.setNext(output.first);
+                output.first = newNode;
+            }
+            n = n.getNext();
+
+        }
+
+        return output;
+    }
+
+    public static void main(String[] args) {
+        Queue queue = new Queue();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        ReverseList r = new ReverseList();
+
+        Queue output = r.reverse(queue);
+        output.printQueue();
+    }
+}
