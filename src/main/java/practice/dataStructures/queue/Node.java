8d37f5ff969f4d96175c141f7f3040b2ecb11611
Yashraj R. Sontakke <sontakkeyashraj@gmail.com>
2015-02-17 23:11:46 -0600

    Implement queue data structure

---
+++ src/main/java/practice/dataStructures/queue/Node.java
@@ -0,0 +1,27 @@
+package practice.dataStructures.queue;
+
+/**
+ * Object for holding node information.
+ *
+ * @author Yashraj R. Sontakke
+ */
+public class Node {
+    public Node next;
+    public final Object data;
+
+    public Node(final Object data) {
+        this.data = data;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public Object getData() {
+        return data;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
+}
---
+++ src/main/java/practice/dataStructures/queue/Queue.java
@@ -0,0 +1,58 @@
+package practice.dataStructures.queue;
+
+import java.util.ArrayList;
+
+/**
+ * Implements a queue.
+ *
+ * @author Yashraj R. Sontakke
+ */
+public class Queue {
+   public Node first, last;
+
+    public void enqueue(Object data) {
+        if (first == null) {
+            last = new Node(data);
+            first = last;
+        } else {
+            last.setNext(new Node(data));
+            last = last.getNext();
+        }
+    }
+
+    public Object dequeue() {
+        if (first != null) {
+            Object data = first.getData();
+            first = first.getNext();
+            return data;
+        }
+        return null;
+    }
+
+    public Object peek() {
+        return first.getData();
+    }
+
+    public void printQueue() {
+        ArrayList<Object> list = new ArrayList<Object>();
+        Node n = first;
+        while (n != null) {
+            list.add(n.getData());
+            n = n.getNext();
+        }
+        System.out.println(list.toString());
+    }
+
+    public static void main(String[] args) {
+        Queue queue = new Queue();
+        queue.printQueue();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+        queue.printQueue();
+        System.out.println(queue.dequeue());
+        queue.peek();
+        queue.printQueue();
+    }
+}
