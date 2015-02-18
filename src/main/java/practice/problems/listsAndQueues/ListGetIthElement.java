ae736eb4464adb4ed0e07900f83af487ac9e6cac
Yashraj R. Sontakke <sontakkeyashraj@gmail.com>
2015-02-17 23:12:03 -0600

    Get the with element from the tail of a list

---
+++ src/main/java/practice/problems/listsAndQueues/ListGetIthElement.java
@@ -0,0 +1,38 @@
+package practice.problems.listsAndQueues;
+
+import practice.dataStructures.queue.Node;
+import practice.dataStructures.queue.Queue;
+
+/**
+ * Retrieves the ith element from the tail of the list.
+ *
+ * @author Yashraj R. Sontakke
+ */
+public class ListGetIthElement {
+
+    public Object getIthInteger(Queue queue, int i){
+        Node fastNode = queue.first;
+        for (int k =0;k<i;k++){
+            fastNode = fastNode.getNext();
+        }
+        Node iNode = queue.first;
+        while (fastNode!=null){
+            fastNode = fastNode.getNext();
+            iNode = iNode.getNext();
+        }
+        return iNode.getData();
+    }
+
+    public static void main(String[] args){
+        Queue queue = new Queue();
+        queue.enqueue(1);
+        queue.enqueue(2);
+        queue.enqueue(3);
+        queue.enqueue(4);
+
+        ListGetIthElement l = new ListGetIthElement();
+        System.out.println(l.getIthInteger(queue, 2));
+        System.out.println(l.getIthInteger(queue,3));
+
+    }
+}
