faa928c7d57ab4ca839f26c70aed024a015f74bb
Yashraj R. Sontakke <sontakkeyashraj@gmail.com>
2015-03-08 15:38:46 -0500

    Find all valid parenthesis

---
+++ src/main/java/practice/problemSolving/recursion/Paranthesis_9_6.java
@@ -0,0 +1,43 @@
+package practice.problemSolving.recursion;
+
+import java.util.ArrayList;
+
+/**
+ * Implement an algorithm to print all valid (i.e., properly opened and closed) combinations
+ * of n-pairs of parentheses
+ *
+ * @author Yashraj R. Sontakke
+ */
+public class Paranthesis_9_6 {
+
+    public void addParenthesis(ArrayList<String> list, int leftRem, int rightRem, char[] str, int count){
+        if (leftRem<0 ||rightRem<leftRem){
+            return;
+        }
+        if (leftRem==0 && rightRem==0){
+            String s = String.copyValueOf(str);
+            list.add(s);
+        }else{
+            if (leftRem >0){
+                str[count] = '(';
+                addParenthesis(list, leftRem-1, rightRem, str, count+1);
+            }
+            if (rightRem > leftRem){
+                str[count] = ')';
+                addParenthesis(list, leftRem, rightRem-1, str, count+1);
+            }
+        }
+    }
+
+    public ArrayList<String> getParens(int count){
+        char[] str = new char[count*2];
+        ArrayList<String> list = new ArrayList<String>();
+        addParenthesis(list, count, count, str,0);
+        return list;
+    }
+
+    public static void main(String[] args){
+        Paranthesis_9_6 permutationsString = new Paranthesis_9_6();
+        System.out.println(permutationsString.getParens(3).toString());
+    }
+}
