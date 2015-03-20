f7842ca3a0f30cb3ba742e201254ad0a51777936
Yashraj R. Sontakke <sontakkeyashraj@gmail.com>
2015-03-19 22:42:24 -0500

    Convert decimal to hexadecimal

---
+++ src/main/java/practice/problems/DecimalToHexadecimal.java
@@ -0,0 +1,33 @@
+package practice.problems;
+
+/**
+ * Convert decimal to hexadecimal.
+ * 10 --> A
+ * 20 --> 14
+ * 31 --> 1F
+ *
+ * @author Yashraj R. Sontakke
+ */
+public class DecimalToHexadecimal {
+
+    char[] hex = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
+    public void convert(int input) {
+        String output = "";
+        int rem;
+        while (input > 0) {
+            rem = input % 16;
+            output = hex[rem] + output;
+            input = input / 16;
+        }
+
+        System.out.println(output);
+    }
+
+    public static void main(String[] args) {
+        DecimalToHexadecimal decimalToHexadecimal = new DecimalToHexadecimal();
+        decimalToHexadecimal.convert(10);
+        decimalToHexadecimal.convert(20);
+        decimalToHexadecimal.convert(31);
+    }
+}
