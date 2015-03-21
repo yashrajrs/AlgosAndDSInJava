ac74e3d51142ecca8016fb9a93518ecd2208a1e2
Yashraj R. Sontakke <sontakkeyashraj@gmail.com>
2015-03-21 17:13:08 -0500

    Check if a string is substring of a rotated string

---
+++ src/main/java/practice/problemSolving/string/StringRotationCheck.java
@@ -0,0 +1,30 @@
+package practice.problemSolving.string;
+
+/**
+ * Assume you have a method isSubstring which checks if one word is a substring
+ * of another. Given two strings, si and s2, write code to check Ifs2 is a rotation of si
+ * using only one call to isSubstring (e.g., "waterbottLe" is a rotation of "erbottLewat").
+ *
+ * @author Yashraj R. Sontakke
+ */
+public class StringRotationCheck {
+
+    public boolean isRotation(String s1, String s2) {
+        if (s1.length() > 0 && s1.length() == s2.length()) {
+            s1 = s1 + s1;
+            return isSubString(s1, s2);
+        }
+        return false;
+    }
+
+    private boolean isSubString(String s1, String s2) {
+        return s1.contains(s2);
+    }
+
+
+    public static void main(String[] args) {
+        StringRotationCheck stringRotationCheck = new StringRotationCheck();
+        System.out.println(stringRotationCheck.isRotation("erbottLewat", "waterbottLe"));
+        System.out.println(stringRotationCheck.isRotation("erbottLewat", "waterbottL"));
+    }
+}
