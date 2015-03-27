98edfdfa617ba617873cdc7b0e1451a98ece4a55
Yashraj R. Sontakke <sontakkeyashraj@gmail.com>
2015-03-26 22:27:34 -0500

    Find median of numbers

--- src/main/java/practice/problems/others/MedianOfNumbers.java
+++ src/main/java/practice/problems/others/MedianOfNumbers.java
@@ -1,7 +1,8 @@
 package practice.problems.others;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
-import java.util.PriorityQueue;
 
 /**
  * Numbers are randomly generated and passed to a method. Write a program to find
@@ -11,37 +12,47 @@ import java.util.PriorityQueue;
  */
 public class MedianOfNumbers {
 
-    private PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(100, new MinHeapComparator());
-    private PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>(100, new MinHeapComparator());
+    private ArrayList<Integer> lowPQ = new ArrayList<Integer>();
+    private ArrayList<Integer> highPQ = new ArrayList<Integer>();
 
-
-    public void addNumber(int randomNumber) {
-        if (maxHeap.size() == minHeap.size()) {
-            if (minHeap.peek() != null && randomNumber > minHeap.peek()) {
-                maxHeap.offer(minHeap.poll());
-                minHeap.offer(randomNumber);
+    public void addElement(int randomNumber) {
+        if (highPQ.isEmpty()) {
+            highPQ.add(randomNumber);
+        } else if (lowPQ.isEmpty()) {
+            if (randomNumber > highPQ.get(highPQ.size() - 1)) {
+                lowPQ.add(highPQ.remove(highPQ.size() - 1));
+                highPQ.add(randomNumber);
             } else {
-                maxHeap.offer(randomNumber);
+                lowPQ.add(randomNumber);
+            }
+        } else if (highPQ.size() == lowPQ.size()) {
+            if (randomNumber > highPQ.get(highPQ.size() - 1)) {
+                highPQ.add(randomNumber);
+            } else {
+                highPQ.add(lowPQ.remove(0));
+                lowPQ.add(randomNumber);
             }
         } else {
-            if (randomNumber < maxHeap.peek()) {
-                minHeap.offer(maxHeap.poll());
-                maxHeap.offer(randomNumber);
+            if (randomNumber > highPQ.get(highPQ.size() - 1)) {
+                lowPQ.add(highPQ.remove(highPQ.size() - 1));
+                highPQ.add(randomNumber);
             } else {
-                minHeap.offer(randomNumber);
+                lowPQ.add(randomNumber);
             }
         }
+        Collections.sort(lowPQ, new MinHeapComparator());
+        Collections.sort(highPQ, new MinHeapComparator());
     }
 
 
     public double getMedian() {
-        if (maxHeap.isEmpty()) {
+        if (highPQ.isEmpty()) {
             return 0;
         }
-        if (maxHeap.size() == minHeap.size()) {
-            return ((double) maxHeap.peek() + minHeap.peek()) / 2;
+        if (highPQ.size() == lowPQ.size()) {
+            return ((double) lowPQ.get(0) + highPQ.get(highPQ.size() - 1)) / 2;
         } else {
-            return maxHeap.peek();
+            return highPQ.get(highPQ.size() - 1);
         }
     }
 
@@ -61,12 +72,12 @@ public class MedianOfNumbers {
 
     public static void main(String[] args) {
         MedianOfNumbers medianOfNumbers = new MedianOfNumbers();
-        medianOfNumbers.addNumber(10);
-        medianOfNumbers.addNumber(15);
-        medianOfNumbers.addNumber(20);
-        medianOfNumbers.addNumber(5);
-        medianOfNumbers.addNumber(30);
-        medianOfNumbers.addNumber(40);
+        medianOfNumbers.addElement(10);
+        medianOfNumbers.addElement(15);
+        medianOfNumbers.addElement(20);
+        medianOfNumbers.addElement(5);
+        medianOfNumbers.addElement(30);
+        medianOfNumbers.addElement(40);
 
         System.out.println(medianOfNumbers.getMedian());
     }
