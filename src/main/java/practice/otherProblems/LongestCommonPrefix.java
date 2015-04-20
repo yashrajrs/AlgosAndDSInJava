4b2c1c2e3e84f7b4af0ea62993d0cb0d4d6b07b3
Yashraj R. Sontakke <sontakkeyashraj@gmail.com>
2015-04-19 21:34:42 -0500

    Longest common prefix

---
+++ src/main/java/practice/otherProblems/LongestCommonPrefix.java
@@ -0,0 +1,52 @@
+package practice.otherProblems;
+
+/**
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ *
+ * @author Yashraj R. Sontakke
+ */
+public class LongestCommonPrefix {
+
+    public String longestCommonPrefix(String[] strs) {
+        if(strs == null || strs.length == 0){
+            return "";
+        }
+        int minLen = Integer.MAX_VALUE;
+        for(String str: strs){
+            if(minLen > str.length()){
+                minLen = str.length();
+            }
+        }
+        if(minLen==0){
+            return "";
+        }
+
+        for(int j=0;j<minLen;j++){
+            char prev = '0';
+            for(int i=0;i<strs.length;i++){
+                if(i==0){
+                    prev = strs[i].charAt(j);
+                    continue;
+                }
+
+                if(prev != strs[i].charAt(j)){
+                    return strs[0].substring(0, j);
+                }
+            }
+        }
+        return strs[0].substring(0, minLen);
+    }
+
+    public static void main(String[] args){
+        LongestCommonPrefix l = new LongestCommonPrefix();
+
+        String[] input1 = {"yash", "yashraj", "yas"};
+        System.out.println(l.longestCommonPrefix(input1));
+
+        String[] input2 = {"yash", "yash"};
+        System.out.println(l.longestCommonPrefix(input2));
+
+        String[] input3= {"yash", "raj"};
+        System.out.println(l.longestCommonPrefix(input3));
+    }
+}
