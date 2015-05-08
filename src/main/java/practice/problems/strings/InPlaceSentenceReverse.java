aeaa9ff33c321a3cf2cfd3ba551d89baccb75dd5
Yashraj R. Sontakke <sontakkeyashraj@gmail.com>
2015-05-07 22:16:06 -0500

    sentence reverse

--- src/main/java/practice/problems/strings/InPlaceSentenceReverse.java
+++ src/main/java/practice/problems/strings/InPlaceSentenceReverse.java
@@ -55,9 +55,24 @@ public class InPlaceSentenceReverse {
         }
     }
 
+    public String reverseWords(String s) {
+        if (s == null || s.length() == 0){
+            return  "";
+        }
+        String[] arr = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (int i = arr.length -1; i>=0; i--){
+            if (!arr[i].equals("")){
+                sb.append(arr[i]).append(" ");
+            }
+        }
+        return sb.length() == 0 ? "" : sb.substring(0, sb.length() -1);
+    }
+
    public static void main(String[] args){
        String input =  "I wish you a merry Christmas";
        InPlaceSentenceReverse inPlaceSentenceReverse =new InPlaceSentenceReverse();
        inPlaceSentenceReverse.reverseSentence(input);
+       System.out.println(inPlaceSentenceReverse.reverseWords(input));
    }
 }
