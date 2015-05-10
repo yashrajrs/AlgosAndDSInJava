cdc2fcb692ac635a5f3d8691aeb39adb247465e7
Yashraj R. Sontakke <sontakkeyashraj@gmail.com>
2015-05-09 19:41:28 -0500

    square root

--- src/main/java/practice/problems/others/SquareRoot.java
+++ src/main/java/practice/problems/others/SquareRoot.java
@@ -22,9 +22,32 @@ public class SquareRoot {
         return mid;
     }
 
+
+    public int mySqrt(int x) {
+        if(x < 2){
+            return x;
+        }
+        int left =1;
+        int right = x/2;
+        while(left <= right){
+            int mid = (left + right)/2;
+            if(x/mid == mid){
+                return mid;
+            }else if(x/mid > mid){
+                left = mid+1;
+            }else{
+                right = mid-1;
+            }
+        }
+        return right;
+    }
+
     public static void main(String[] args) {
         SquareRoot squareRoot = new SquareRoot();
         System.out.println(String.format("Square root of %d = %.2f", 9, squareRoot.getSquareRoot(9)));
         System.out.println(String.format("Square root of %d = %.2f", 8, squareRoot.getSquareRoot(8)));
+        System.out.println(String.format("Square root of %d = %d", 9, squareRoot.mySqrt(9)));
+        System.out.println(String.format("Square root of %d = %d", 8, squareRoot.mySqrt(8)));
+
     }
 }
